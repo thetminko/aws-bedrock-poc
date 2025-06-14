17db76b3de69886e0a129cc63aa51e4022f6a386
Thet Min Ko <dev.minko@gmail.com>
2025-06-13 23:21:48 -0400

    Connect with aws bedrock and secret managed by 1pass

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,17 @@
-console.log('Hello, world!');
+import { BedrockClient, ListFoundationModelsCommand } from "@aws-sdk/client-bedrock";
+
+const client = new BedrockClient({
+  region: 'ca-central-1'
+})
+
+async function listFoundationModels() {
+  const response = await client.send(new ListFoundationModelsCommand())
+  response.modelSummaries?.map(model => {
+    console.log(`Model ${model.modelId} - ${model.modelName}`);
+  });
+}
+
+listFoundationModels()
+  .then(() => console.log('Done'))
+  .catch(err => console.error('Error listing foundation models:', err));
+
